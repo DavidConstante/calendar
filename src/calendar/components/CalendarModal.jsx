e01e329ac94bcca5a198aba2b25f00224cd1892c
DavidConstante <omileconstante@gmail.com>
2022-09-21 00:09:30 -0500

    Fix the error

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -65,89 +65,89 @@ export const CalendarModal = () => {
     if (formValues.title.trim().length < 2) return
 
     //TODO: Close the modal
-
-
-    return (
-      <Modal
-        className={'modal'}
-        overlayClassName={'modal-fondo'}
-        closeTimeoutMS={200}
-        isOpen={isOpen}
-        onRequestClose={onCloseModal}
-        style={customStyles}
-      >
-        <h1> Nuevo evento </h1>
-        <hr />
-        <form
-          className="container"
-          onSubmit={onSubmit}
-        >
-
-          <div className="form-group mb-2">
-            <label>Fecha y hora inicio</label>
-            <DatePicker
-              className="form-control"
-              onChange={(e) => onDateChange(e, 'start')}
-              dateFormat='Pp'
-              showTimeSelect
-              locale={'es'}
-              timeCaption='Hora'
-              selected={formValues.start}
-            />
-          </div>
-
-          <div className="form-group mb-2">
-            <label>Fecha y hora fin</label>
-            <DatePicker
-              minDate={formValues.start}
-              className="form-control"
-              onChange={(e) => onDateChange(e, 'end')}
-              dateFormat='Pp'
-              showTimeSelect
-              locale={'es'}
-              timeCaption='Hora'
-              selected={formValues.end}
-            />
-          </div>
-
-          <hr />
-          <div className="form-group mb-2">
-            <label>Titulo y notas</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Título del evento"
-              name="title"
-              value={formValues.title}
-              autoComplete="off"
-              onChange={onInputChange}
-            />
-            <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
-          </div>
-
-          <div className="form-group mb-2">
-            <textarea
-              type="text"
-              className="form-control"
-              placeholder="Notas"
-              rows="5"
-              name="notes"
-              value={formValues.notes}
-              onChange={onInputChange}
-            ></textarea>
-            <small id="emailHelp" className="form-text text-muted">Información adicional</small>
-          </div>
-
-          <button
-            type="submit"
-            className="btn btn-outline-primary btn-block"
-          >
-            <i className="far fa-save"></i>
-            <span> Guardar</span>
-          </button>
-
-        </form>
-      </Modal>
-    )
   }
+
+  return (
+    <Modal
+      className={'modal'}
+      overlayClassName={'modal-fondo'}
+      closeTimeoutMS={200}
+      isOpen={isOpen}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+    >
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form
+        className="container"
+        onSubmit={onSubmit}
+      >
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <DatePicker
+            className="form-control"
+            onChange={(e) => onDateChange(e, 'start')}
+            dateFormat='Pp'
+            showTimeSelect
+            locale={'es'}
+            timeCaption='Hora'
+            selected={formValues.start}
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <DatePicker
+            minDate={formValues.start}
+            className="form-control"
+            onChange={(e) => onDateChange(e, 'end')}
+            dateFormat='Pp'
+            showTimeSelect
+            locale={'es'}
+            timeCaption='Hora'
+            selected={formValues.end}
+          />
+        </div>
+
+        <hr />
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            value={formValues.title}
+            autoComplete="off"
+            onChange={onInputChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+        </div>
+
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={formValues.notes}
+            onChange={onInputChange}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-outline-primary btn-block"
+        >
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+
+      </form>
+    </Modal>
+  )
+
 }
